295f20456c66576858daa02e62c3e591f846e928
Ashhabur Rahaman <121369728+sahayotasybertech@users.noreply.github.com>
2024-07-02 22:46:28 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,25 +37,6 @@ $(document).ready(function () {
         }, 500, 'linear')
     });
 
-    // <!-- emailjs to mail contact form data -->
-    $("#contact-form").submit(function (event) {
-        emailjs.init({
-            publicKey: "BemwfZh3bVak5dOuj",
-          });
-
-        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
-            .then(function (response) {
-                console.log('SUCCESS!', response.status, response.text);
-                document.getElementById("contact-form").reset();
-                alert("Form Submitted Successfully");
-            }, function (error) {
-                console.log('FAILED...', error);
-                alert("Form Submission Failed! Try Again");
-            });
-        event.preventDefault();
-    });
-    // <!-- emailjs to mail contact form data -->
-
 });
 
 document.addEventListener('visibilitychange',
